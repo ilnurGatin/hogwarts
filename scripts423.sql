7d3e40aba14552318a4776e110431c6f4487cb5d
ilnurGatin <reilag@yandex.ru>
2022-12-22 00:28:34 +0500

    Исправил последний запрос (запрашиваю всех студентов и названия их факультета при условии, что у студента есть аватар)

--- scripts423.sql
+++ scripts423.sql
@@ -1,7 +1,7 @@
-SELECT name.student, age.student, name.faculty
-FROM (SELECT name.student, age.student, student_id.avatar
+SELECT name.new_student, age.new_student, name.faculty
+FROM (SELECT name.student, age.student, faculty_id.student student_id.avatar
       FROM student
       JOIN avatar ON student_id.avatar = id.student
-      WHERE student_id IS NOT NULL)
-JOIN faculty ON faculty_id.student = id.faculty
+      WHERE student_id IS NOT NULL) AS new_student
+JOIN faculty ON faculty_id.new_student = id.faculty
 
