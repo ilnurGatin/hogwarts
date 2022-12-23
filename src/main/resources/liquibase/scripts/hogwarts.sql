c3b7a2641736ba49b1078fee4ab596f4fc5eaac3
ilnurGatin <reilag@yandex.ru>
2022-12-24 00:43:51 +0500

    Внес правки в синтаксис ChangeSet 1 , 2

--- src/main/resources/liquibase/scripts/hogwarts.sql
+++ src/main/resources/liquibase/scripts/hogwarts.sql
@@ -1,7 +1,7 @@
 -- liquibase formatted sql
 
- - changeset igatin:1
+-- changeSet igatin:1
 CREATE INDEX student_name_index ON student (name);
 
- - changeset igatin:2
+-- changeSet igatin:2
 CREATE INDEX faculty_cn_index ON faculty (color, name);
